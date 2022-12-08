aefbcfc518914f567c0a513c909057f8942e0fd6
diego <yodiegocalvo@gmail.com>
2022-12-08 12:50:59 +0100

    media icons update

--- js/script.js
+++ js/script.js
@@ -25,26 +25,45 @@ function funcionOscuro() {
       for (const dropdown of dropdowns) {
                   dropdown.classList.add('dropdown-menu-dark');
                 }
-
   }
   var element = document.body;
   element.classList.toggle("dark-mode-body");
-
 
   var element2 = document.getElementById("my-header");
   element2.classList.toggle("dark-mode-header");
 
     /*var iconUser = document.getElementById("iconUser");
     iconUser.classList.toggle("dark-mode-iconUser");*/
-
-
 }
-// When the user scrolls the page, execute funcionDesplazamiento
-window.onscroll = function() {funcionDesplazamiento()};
+// When the user scrolls the page, execute scrollFunction
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+/*
+Funcion boton volver arriba
+*/
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
 
-function funcionDesplazamiento() {
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function scrollFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
+  if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
 }
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
