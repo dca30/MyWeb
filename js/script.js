b6e30827c38fc67321da497a4e976d35355a028e
dca30 <yodiegocalvo@gmail.com>
2023-01-04 18:20:26 +0100

    navlink active + others

--- js/script.js
+++ js/script.js
@@ -9,6 +9,61 @@ let socialMenu = document.getElementById("social");
 // When the user clicks on the button, scroll to the top of the document
 btnArriba.addEventListener("click", backToTop);
 /*
+ var navlinks = document.querySelectorAll(".navbar-nav a");
+  for (var i = 0; i < navlinks.length; i++) {
+    navlinks[i].classList.remove("active");
+  }
+*//*
+  const currentPage = location.pathname;
+  if (currentPage === '/MyWeb/index.html') {
+    // The user is on the home page
+    const navlink = document.querySelector('a[href="index.html"]');
+    navlink.classList.add('active');
+  } else if (currentPage === '/MyWeb/aboutme.html') {
+    // The user is on the about page
+    const navlink = document.querySelector('a[href="aboutme.html"]');
+    navlink.classList.add('active');
+  } else if (currentPage === '/MyWeb/projects.html') {
+    // The user is on the contact page
+    const navlink = document.querySelector('a[href="projects.html"]');
+    navlink.classList.add('active');
+    } else if (currentPage === '/MyWeb/skills.html') {
+    // The user is on the contact page
+    const navlink = document.querySelector('a[href="skills.html"]');
+    navlink.classList.add('active');
+  }
+*/
+
+const currentPage = location.pathname;
+
+if (currentPage.startsWith('/MyWeb/')) {
+  console.log(currentPage.substring(7)); // Log the name of the HTML file
+  const navlink = document.querySelector(`a[href="${currentPage.substring(7)}"]`);
+
+  switch (currentPage) {
+    case '/MyWeb/index.html':
+    case '/MyWeb/aboutme.html':
+    case '/MyWeb/projects.html':
+    case '/MyWeb/skills.html':
+      navlink.classList.add('active');
+      break;
+    default:
+      break;
+  }
+}
+
+
+/*
+  function setActive(event) {
+      var navlinks = document.querySelectorAll(".navbar-nav a");
+      console.log("Me has clickado");
+      for (var i = 0; i < navlinks.length; i++) {
+        navlinks[i].classList.remove("active");
+      }
+      event.target.classList.add("active");
+    }
+*/
+/*
 var node = document.querySelector('[title="Hosted on free web hosting 000webhost.com. Host your own website for FREE."]');
 node.style.display = "none";
 */
