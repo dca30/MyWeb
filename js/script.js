deaa0644371ec33a8bfd6dfb57bf449a26296c08
dca30 <yodiegocalvo@gmail.com>
2023-01-05 17:15:23 +0100

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // When the user scrolls the page, execute scrollFunction
 // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrolled > 200) {
+    document.getElementById("social").classList.add("show");
+  }
+};
 window.onscroll = function() {scrollFunction()};
 /*Funcion boton volver arriba*/
 //Get the button
@@ -8,36 +14,10 @@ let socialMenu = document.getElementById("social");
 
 // When the user clicks on the button, scroll to the top of the document
 btnArriba.addEventListener("click", backToTop);
-/*
- var navlinks = document.querySelectorAll(".navbar-nav a");
-  for (var i = 0; i < navlinks.length; i++) {
-    navlinks[i].classList.remove("active");
-  }
-*//*
-  const currentPage = location.pathname;
-  if (currentPage === '/MyWeb/index.html') {
-    // The user is on the home page
-    const navlink = document.querySelector('a[href="index.html"]');
-    navlink.classList.add('active');
-  } else if (currentPage === '/MyWeb/aboutme.html') {
-    // The user is on the about page
-    const navlink = document.querySelector('a[href="aboutme.html"]');
-    navlink.classList.add('active');
-  } else if (currentPage === '/MyWeb/projects.html') {
-    // The user is on the contact page
-    const navlink = document.querySelector('a[href="projects.html"]');
-    navlink.classList.add('active');
-    } else if (currentPage === '/MyWeb/skills.html') {
-    // The user is on the contact page
-    const navlink = document.querySelector('a[href="skills.html"]');
-    navlink.classList.add('active');
-  }
-*/
 
 const currentPage = location.pathname;
 
 if (currentPage.startsWith('/MyWeb/')) {
-  console.log(currentPage.substring(7)); // Log the name of the HTML file
   const navlink = document.querySelector(`a[href="${currentPage.substring(7)}"]`);
 
   switch (currentPage) {
@@ -52,17 +32,6 @@ if (currentPage.startsWith('/MyWeb/')) {
   }
 }
 
-
-/*
-  function setActive(event) {
-      var navlinks = document.querySelectorAll(".navbar-nav a");
-      console.log("Me has clickado");
-      for (var i = 0; i < navlinks.length; i++) {
-        navlinks[i].classList.remove("active");
-      }
-      event.target.classList.add("active");
-    }
-*/
 /*
 var node = document.querySelector('[title="Hosted on free web hosting 000webhost.com. Host your own website for FREE."]');
 node.style.display = "none";
@@ -78,7 +47,7 @@ function scrollFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
   if (
-      document.body.scrollTop > 20 ||
+      document.body.scrollTop > 200 ||
       document.documentElement.scrollTop > 20
     ) {
       btnArriba.style.display = "block";
