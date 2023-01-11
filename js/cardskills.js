8af42c6ebf75ea64c030faa0e91d04d2c87a532f
dcalvo <dcalvo@isyc.com>
2023-01-11 17:27:49 +0100

    changing letters + initial progress bar

--- js/cardskills.js
+++ js/cardskills.js
@@ -1,25 +1,53 @@
 const colors = [
-  '#f0c674', // Linux
-  '#0db7ed',  // Docker
-  '#b07219', // Java
-  '#3572a5', // Python
-  '#e34c26', // HTML
-  '#f7df1e', // JavaScript
-  '#563d7c', // Bootstrap
-  '#254BDD', // CSS
-  '#808080', // SQL
-  '#4f5d95', // PHP
-  '#f05340', // Laravel
-  '#f34b7d', // Git
+  "#f0c674", // Linux
+  "#0db7ed", // Docker
+  "#b07219", // Java
+  "#3572a5", // Python
+  "#e34c26", // HTML
+  "#f7df1e", // JavaScript
+  "#563d7c", // Bootstrap
+  "#254BDD", // CSS
+  "#808080", // SQL
+  "#4f5d95", // PHP
+  "#f05340", // Laravel
+  "#f34b7d", // Git
 ];
 
-const transparentColors = colors.map(color => {
-  const [r, g, b] = color.match(/\w\w/g).map(x => parseInt(x, 16));
+const transparentColors = colors.map((color) => {
+  const [r, g, b] = color.match(/\w\w/g).map((x) => parseInt(x, 16));
   return `rgba(${r}, ${g}, ${b}, 0.5)`;
 });
 
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
 cards.forEach((card, index) => {
   card.style.backgroundColor = transparentColors[index];
 });
+changeDisplay = document.getElementById("change-layout-button");
+changeDisplay.addEventListener("click", changeLayout);
+function changeLayout() {
+  var cards = document.querySelectorAll(".carta");
+  cards.forEach(function (card) {
+    card.classList.toggle("col-sm-2");
+    card.classList.toggle("col-sm-12");
+  });
+}
+
+/*window.addEventListener("scroll", function () {
+  var cards = document.querySelectorAll(".card-skill");
+  if (window.scrollY == 0) {
+    cards.forEach(function (card) {
+      card.classList.remove("horizontal");
+      //card.classList.add("card");
+      card.classList.add("card-skill");
+      card.classList.add("pt-4");
+    });
+  } else {
+    cards.forEach(function (card) {
+      card.classList.add("horizontal");
+      //card.classList.remove("card");
+      card.classList.remove("card-skill");
+      card.classList.remove("pt-4");
+    });
+  }
+});*/
---
+++ js/title.js
@@ -0,0 +1,75 @@
+/*$(document).ready(function() {
+  var randomnbr = $('.nbr');
+  var timer = 30;
+  var it;
+  var data = 0;
+  var index;
+  var change;
+  var letters = ["c", "o", "d", "e", "g", "r", "i", "d"];
+
+  randomnbr.each(function() {
+  	
+    change = Math.round(Math.random()*100);
+    $(this).attr("data-change", change);
+  	
+  });
+	
+  function random() {return Math.round(Math.random() * 9);};
+  function select() {return Math.round(Math.random() * randomnbr.length + 1);};
+  function value() {
+    $('.nbr:nth-child('+select()+')').html(''+random()+'');
+    $('.nbr:nth-child('+select()+')').attr('data-number', data);
+    data++;
+  	
+    randomnbr.each(function() {
+      if(parseInt($(this).attr('data-number')) > parseInt($(this).attr('data-change'))) {
+        index = $('.ltr').index(this);
+        $(this).html(letters[index]);
+        $(this).removeClass('nbr');
+      }
+    });
+  };
+  $it = setInterval(value, timer);
+});
+*/ /*function random() {
+          return Math.round(Math.random() * 9);
+        }*/
+/*function select() {
+    return Math.floor(Math.random() * randomnbr.length);
+  }*/
+document.addEventListener("DOMContentLoaded", function () {
+  var randomnbr = document.querySelectorAll(".nbr");
+  var timer = 50;
+  var inc = 10;
+  var data = 0;
+  var count = 0;
+  var it, index, change;
+  var letters = ["d", "i", "e", "g", "o", " ", "c", "a", "l", "v", "o"];
+
+  randomnbr.forEach(function (el) {
+    change = Math.round(Math.random() * 100);
+    el.setAttribute("data-change", change);
+  });
+  
+  function value() {
+    var index;
+    var randomEl = randomnbr[Math.floor(Math.random() * randomnbr.length)];
+    randomEl.innerHTML = Math.round(Math.random() * 9);
+    randomEl.setAttribute("data-number", data);
+    data++;
+
+    randomnbr.forEach(function (el) {
+      if (
+        parseInt(el.getAttribute("data-number")) <
+        parseInt(el.getAttribute("data-change"))
+      ) {
+        index = Array.from(randomnbr).indexOf(el);
+        el.innerHTML = letters[index];
+        el.classList.remove("nbr");
+        count++;
+      }
+    });
+    setTimeout(value, timer + inc * count);
+  }
+  it = setInterval(value, timer);
+});
