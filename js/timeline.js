17099a3e84b26c333e3812e3ee7643e86ce625f6
dca30 <yodiegocalvo@gmail.com>
2023-01-20 20:42:42 +0100

    mucho commit poco cambio

--- js/timeline.js
+++ js/timeline.js
@@ -10,7 +10,7 @@ const words1 = [
   "Automatizacion",
 ];
 const icons1 = [
-  [("fas", "fa-shield-alt")],
+  ["fas", "fa-shield-alt"],
   ["fas", "fa-server"],
   ["fas", "fa-code"],
   ["fas", "fa-database"],
@@ -34,7 +34,7 @@ const words2 = [
   "peliculas",
 ];
 const icons2 = [
-  ["bx", "bx-tennis-ball"],
+  ["fas", "fa-tennis-ball"],
   ["fas", "fa-paddle-ball"],
   ["fas", "fa-dumbbell"],
   ["fas", "fa-bicycle"],
@@ -50,24 +50,24 @@ createTimeline(words1, container1, icons1);
 createTimeline(words2, container2, icons2);
 
 function createTimeline(words, container, icons) {
-  createCirclesAndLines(words, container);
-  // Add click event listener to circles
+  createDotsAndLines(words, container);
+  // Add click event listener to dots
   let lastClicked;
-  const circles = document.querySelectorAll(".dot");
-  circles.forEach((circle) => {
-    addClickEventToCircle(circle);
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot) => {
+    addClickEventToDot(dot);
   });
 
-  function createCirclesAndLines(words, container) {
+  function createDotsAndLines(words, container) {
     // Create start line
     const startLine = document.createElement("span");
     startLine.classList.add("line");
     container.prepend(startLine);
 
-    // Create circles for each word
+    // Create dots for each word
     words.forEach((word, index) => {
-      const circle = createCircle(word, index);
-      container.appendChild(circle);
+      const dot = createDot(word, index);
+      container.appendChild(dot);
       index < words.length - 1 ? container.appendChild(createLine()) : null;
     });
 
@@ -76,16 +76,16 @@ function createTimeline(words, container, icons) {
     endLine.classList.add("line");
     container.appendChild(endLine);
   }
-  // Create circle
-  function createCircle(word, index) {
-    const circle = document.createElement("div");
-    circle.classList.add("dot");
+  // Create dot
+  function createDot(word, index) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
 
     const diagonal = document.createElement("p");
     diagonal.classList.add("diagonal");
     diagonal.innerText = word;
 
-    // Add Font Awesome icon to circle
+    // Add Font Awesome icon to dot
     const iconClasses = icons[index];
     icon = iconClasses
       ? ((icon = document.createElement("i")),
@@ -93,9 +93,9 @@ function createTimeline(words, container, icons) {
         icon)
       : null;
 
-    circle.appendChild(icon);
-    circle.appendChild(diagonal);
-    return circle;
+    dot.appendChild(icon);
+    dot.appendChild(diagonal);
+    return dot;
   }
   // Create line
   function createLine() {
@@ -103,39 +103,43 @@ function createTimeline(words, container, icons) {
     line.classList.add("line");
     return line;
   }
-  // Add click event to circle
-  function addClickEventToCircle(circle) {
-    circle.addEventListener("click", function () {
-      lastClicked
-        ? (lastClicked.classList.toggle("bigger"),
-          lastClicked.classList.remove("bold"),
-          lastClicked.querySelector(".icon")
-            ? lastClicked.querySelector(".icon").classList.add("hide")
-            : null)
-        : null;
-      circle.classList.add("bigger");
-      circle.classList.contains("bigger")
-        ? circle.querySelector(".icon").classList.remove("hide")
-        : circle.querySelector(".icon").classList.add("hide");
-      circle.style.backgroundColor = "black";
-      circle.querySelector(".diagonal").classList.add("bold");
-      lastClicked
-        ? lastClicked.querySelector(".diagonal").classList.remove("bold")
-        : null;
-      lastClicked = circle;
+  // Add click event to dot
+  function addClickEventToDot(dot) {
+    dot.addEventListener("click", function () {
+      if (lastClicked === dot) {
+        return;
+      }
+      if (lastClicked) {
+        lastClicked.classList.toggle("bigger");
+        if (lastClicked.querySelector(".icon")) {
+          lastClicked.querySelector(".icon").classList.add("hide");
+        }
+      }
+      dot.classList.add("bigger");
+      if (dot.classList.contains("bigger")) {
+        dot.querySelector(".icon").classList.remove("hide");
+      } else {
+        dot.querySelector(".icon").classList.add("hide");
+      }
+      dot.style.backgroundColor = "black";
+      dot.querySelector(".diagonal").classList.add("bold");
+      if (lastClicked) {
+        lastClicked.querySelector(".diagonal").classList.remove("bold");
+      }
+      lastClicked = dot;
 
-      let prev = circle.previousElementSibling;
+      let prev = dot.previousElementSibling;
       while (prev) {
-        prev.classList.contains("dot") || prev.classList.contains("line")
-          ? (prev.style.backgroundColor = "black")
-          : null;
+        if (prev.classList.contains("dot") || prev.classList.contains("line")) {
+          prev.style.backgroundColor = "black";
+        }
         prev = prev.previousElementSibling;
       }
-      let next = circle.nextElementSibling;
+      let next = dot.nextElementSibling;
       while (next) {
-        next.classList.contains("dot") || next.classList.contains("line")
-          ? (next.style.backgroundColor = "lightgray")
-          : null;
+        if (next.classList.contains("dot") || next.classList.contains("line")) {
+          next.style.backgroundColor = "lightgray";
+        }
         next = next.nextElementSibling;
       }
     });
