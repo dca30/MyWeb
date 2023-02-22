ee0a27b4f8f842b7ad614c8071f3f8ac3a356c2b
Diego Calvo Alegre <dcalvo@isyc.com>
2023-02-22 15:26:41 +0100

    timeline improve try

--- js/timeline.js
+++ js/timeline.js
@@ -1,4 +1,4 @@
-//Primera timeline
+/*//Primera timeline
 const container1 = document.querySelector(".container1-timeline");
 const words1 = [
   "Cyber",
@@ -76,14 +76,14 @@ function createTimeline(words, container, icons) {
     dot.classList.add("dot");
 
     const diagonal = document.createElement("p");
-    diagonal.classList.add("diagonal");
+    diagonal.classList.add("diagonal", "hide"); // add the hide class here
     diagonal.innerText = word;
 
     // Add Font Awesome icon to dot
     const iconClasses = icons[index];
     icon = iconClasses
       ? ((icon = document.createElement("i")),
-        icon.classList.add(...iconClasses, "icon", "hide"),
+        icon.classList.add(...iconClasses, "icon"),
         icon)
       : null;
 
@@ -91,6 +91,7 @@ function createTimeline(words, container, icons) {
     dot.appendChild(diagonal);
     return dot;
   }
+
   // Create line
   function createLine() {
     const line = document.createElement("span");
@@ -105,15 +106,15 @@ function createTimeline(words, container, icons) {
       }
       if (lastClicked) {
         lastClicked.classList.toggle("bigger");
-        if (lastClicked.querySelector(".icon")) {
-          lastClicked.querySelector(".icon").classList.add("hide");
+        if (lastClicked.querySelector(".diagonal")) {
+          lastClicked.querySelector(".diagonal").classList.add("hide");
         }
       }
       dot.classList.add("bigger");
       if (dot.classList.contains("bigger")) {
-        dot.querySelector(".icon").classList.remove("hide");
+        dot.querySelector(".diagonal").classList.remove("hide");
       } else {
-        dot.querySelector(".icon").classList.add("hide");
+        dot.querySelector(".diagonal").classList.add("hide");
       }
       dot.style.backgroundColor = "black";
       dot.querySelector(".diagonal").classList.add("bold");
@@ -138,4 +139,144 @@ function createTimeline(words, container, icons) {
       }
     });
   }
+}
+*/
+function createTimeline(words, container, icons) {
+  createDotsAndLines(words, container);
+
+  function createDotsAndLines(words, container) {
+    // Create start line
+    const startLine = document.createElement("span");
+    startLine.classList.add("line");
+    container.prepend(startLine);
+
+    // Create dots for each word
+    words.forEach((word, index) => {
+      const dot = createDot(word, index);
+      container.appendChild(dot);
+      index < words.length - 1 ? container.appendChild(createLine()) : null;
+    });
+
+    // Create end line
+    const endLine = document.createElement("span");
+    endLine.classList.add("line");
+    container.appendChild(endLine);
+  }
+
+  function createDot(word, index) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+
+    const diagonal = document.createElement("p");
+    diagonal.classList.add("diagonal", "hide");
+    diagonal.innerText = word;
+
+    // Add Font Awesome icon to dot
+    const iconClasses = icons[index];
+    icon = iconClasses
+      ? ((icon = document.createElement("i")),
+        icon.classList.add(...iconClasses, "icon"),
+        icon)
+      : null;
+
+    dot.appendChild(icon);
+    dot.appendChild(diagonal);
+    return dot;
+  }
+
+  function createLine() {
+    const line = document.createElement("span");
+    line.classList.add("line");
+    return line;
+  }
+}
+const dots = document.querySelectorAll(".dot");
+
+// Example usage:
+const container1 = document.querySelector(".container1-timeline");
+const words1 = [
+  "Cyber",
+  "Servidores",
+  "Frontend",
+  "Backend",
+  "Linux",
+  "OS",
+  "Auto",
+];
+const icons1 = [
+  ["fas", "fa-shield-alt"],
+  ["fas", "fa-server"],
+  ["fas", "fa-code"],
+  ["fas", "fa-database"],
+  ["fa-brands", "fa-linux"],
+  ["fas", "fa-laptop-code"],
+  ["fas", "fa-robot"],
+];
+createTimeline(words1, container1, icons1);
+
+const container2 = document.querySelector(".container2-timeline");
+const words2 = [
+  "Tenis",
+  "Padel",
+  "Gym",
+  "Bici",
+  "Correr",
+  "Electro",
+  "Series",
+  "Peliculas",
+];
+const icons2 = [
+  ["fas", "fa-tennis-ball"],
+  ["fas", "fa-paddle-ball"],
+  ["fas", "fa-dumbbell"],
+  ["fas", "fa-bicycle"],
+  ["fas", "fa-running"],
+  ["fas", "fa-plug"],
+  ["fas", "fa-tv"],
+  ["fas", "fa-film"],
+];
+createTimeline(words2, container2, icons2);
+dots.forEach((dot) => {
+  addClickEventToDot(dot);
+});
+function addClickEventToDot(dot) {
+  dot.addEventListener("click", function () {
+    console.log("clickado el que tengo aqui colgado");
+    if (lastClicked === dot) {
+      return;
+    }
+    if (lastClicked) {
+      lastClicked.classList.toggle("bigger");
+      if (lastClicked.querySelector(".diagonal")) {
+        lastClicked.querySelector(".diagonal").classList.add("hide");
+      }
+    }
+    dot.classList.add("bigger");
+    if (dot.classList.contains("bigger")) {
+      dot.querySelector(".diagonal").classList.remove("hide");
+    } else {
+      dot.querySelector(".diagonal").classList.add("hide");
+    }
+    dot.style.backgroundColor = "black";
+    dot.querySelector(".diagonal").classList.add("bold");
+    if (lastClicked) {
+      lastClicked.querySelector(".diagonal").classList.remove("bold");
+    }
+    lastClicked = dot;
+
+    let prev = dot.previousElementSibling;
+    while (prev) {
+      if (prev.classList.contains("dot") || prev.classList.contains("line")) {
+        prev.style.backgroundColor = "black";
+      }
+      prev = prev.previousElementSibling;
+    }
+    let next = dot.nextElementSibling;
+    while (next) {
+      if (next.classList.contains("dot") || next.classList.contains("line")) {
+        next.style.backgroundColor = "lightgray";
+      }
+      next = next.nextElementSibling;
+    }
+  });
 }
