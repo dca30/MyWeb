13a78bfcb8a2d030f0b8dc4bb8e387c42cbcb9cc
Diego Calvo Alegre <dcalvo@isyc.com>
2023-02-24 17:27:40 +0100

    dark mode fixes

--- js/dark.js
+++ js/dark.js
@@ -1,28 +1,25 @@
 const body = document.querySelector("body");
 const toggle = document.getElementById("toggle");
-const timeline = document.getElementById("cardTimeline");
+//const timeline = document.getElementById("cardTimeline");
 const sunIcon = document.querySelector(".toggle .bxs-sun");
 const moonIcon = document.querySelector(".toggle .bx-moon");
 const navTop = document.getElementById("navTop");
-const dropdowns = document.querySelectorAll(".dropdown-menu");
+//const dropdown = document.querySelector(".dropdown-menu");
 
 function toggleDarkMode() {
   body.classList.toggle("dark");
   body.classList.toggle("light");
-  body.classList.toggle("dark-body");
+
   navTop.classList.toggle("navbar-dark");
   navTop.classList.toggle("navbar-light");
+
   sunIcon.className =
     sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
   moonIcon.className =
     moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-  Array.from(document.getElementsByClassName("card")).forEach((card) => {
-    card.classList.toggle("dark-mode-card");
-  });
-  if (timeline) timeline.classList.toggle("dark-mode-card");
-  dropdowns.forEach((dropdown) => {
-    dropdown.classList.toggle("dropdown-menu-dark");
-  });
+
+  //if (timeline) timeline.classList.toggle("dark-mode-card");
+  //dropdown.classList.toggle("dropdown-menu-dark");
   localStorage.setItem("dark", body.classList.contains("dark"));
 }
 document.addEventListener("DOMContentLoaded", function () {
