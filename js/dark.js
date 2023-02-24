d1fb76154ab6b1673f7066a02eff534c76edfc28
dca30 <yodiegocalvo@gmail.com>
2023-02-24 19:41:38 +0100

    dark mode almost fixed

--- js/dark.js
+++ js/dark.js
@@ -10,16 +10,16 @@ function toggleDarkMode() {
   body.classList.toggle("dark");
   body.classList.toggle("light");
 
-  navTop.classList.toggle("navbar-dark");
-  navTop.classList.toggle("navbar-light");
+  //navTop.classList.toggle("navbar-dark");
+  //navTop.classList.toggle("navbar-light");
 
   sunIcon.className =
     sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
   moonIcon.className =
     moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
 
-  //if (timeline) timeline.classList.toggle("dark-mode-card");
-  //dropdown.classList.toggle("dropdown-menu-dark");
+  /*if (timeline) timeline.classList.toggle("dark-mode-card");
+  dropdown.classList.toggle("dropdown-menu-dark");*/
   localStorage.setItem("dark", body.classList.contains("dark"));
 }
 document.addEventListener("DOMContentLoaded", function () {
